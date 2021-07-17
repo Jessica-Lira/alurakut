1b97145b29e97e398c3884517a89e16351958868
Jessica-Lira <contatojlira@gmail.com>
2021-07-16 21:05:41 -0300

    corrige alguns links do gituser

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home(props) {
   //Pega o array de dados do github
   const [seguidores, setSeguidores] = React.useState([]);
   React.useEffect(async function () {
-     fetch("https://api.github.com/users/Jessica-Lira/followers")
+     fetch(`https://api.github.com/users/${githubUser}/followers`)
        .then((respostaDoServidor) => {
          return respostaDoServidor.json();
        })
